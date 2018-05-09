01a887cce74e396a4849e2f4ca9aeaa95ad712e0
alexey podvoyskiy <fell978@yandex.ru>
2018-05-09 09:32:09 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,35 +69,28 @@ BOARD_WANTS_EMMC_BOOT := true
 # Video
 COMMON_GLOBAL_CFLAGS += -DQCOM_BSP
 
-# FM Radio
-BOARD_HAVE_FM_RADIO := true
-BOARD_FM_DEVICE := bcm4330
-BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
-
 # Qualcomm hardware
 BOARD_USES_QCOM_HARDWARE := true
 BOARD_USES_QCOM_LIBS := true
 COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
 
 # Wi-Fi
-BOARD_WLAN_DEVICE := bcmdhd
-WPA_SUPPLICANT_VERSION := VER_0_8_X
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
 BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-BOARD_LEGACY_NL80211_STA_EVENTS := true
-WIFI_DRIVER_FW_PATH_AP := "/system/etc/fw_4330_b2.bin"
-WIFI_DRIVER_FW_PATH_STA := "/system/etc/fw_4330_b2.bin"
-WIFI_DRIVER_FW_PATH_P2P := "/system/etc/fw_4330_b2.bin"
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WLAN_DEVICE := bcmdhd
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_DRIVER_FW_PATH_AP := "/vendor/firmware/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_STA := "/vendor/firmware/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_P2P := "/vendor/firmware/fw_bcmdhd_apsta.bin"
 WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/dhd/parameters/firmware_path"
-WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/dhd.ko"
-WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/fw_4330_b2.bin nvram_path=/system/etc/nvram_4330.txt"
+WIFI_DRIVER_MODULE_ARG := "firmware_path=${WIFI_DRIVER_FW_PATH_STA} nvram_path=/system/etc/nvram_4330.txt"
 WIFI_DRIVER_MODULE_NAME := "dhd"
-WIFI_EXT_MODULE_PATH := "/system/lib/modules/cfg80211.ko"
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/${WIFI_DRIVER_MODULE_NAME}.ko"
 WIFI_EXT_MODULE_NAME := "cfg80211"
-
-TARGET_CUSTOM_WIFI := ../../device/huawei/u8950/libhardware_legacy/wifi/wifi.c
+WIFI_EXT_MODULE_PATH := "/system/lib/modules/${WIFI_EXT_MODULE_NAME}.ko"
+BOARD_LEGACY_NL80211_STA_EVENTS := true
 
 # Audio
 TARGET_PROVIDES_LIBAUDIO := true
